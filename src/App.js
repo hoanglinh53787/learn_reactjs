3295dd792506dc6b60481f4ec9fa3b1cdcd1e2eb
Hoang Linh <hoanglinh53787@gmail.com>
2022-01-31 04:18:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Linh Nguye
+          Linh Nguyen
         </p>
         <a
           className="App-link"
